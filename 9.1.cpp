dd2c273c5f4474dab7459eb7ab89be4dfeb0d5de
OlegChaichenko <113429945+OlegChaichenko@users.noreply.github.com>
2022-11-21 16:16:54 +0200

    Oleg Chaichenko,comp math 1,9.1-9.4
    Oleg Chaichenko,comp math 1,9.1-9.4

---
+++ 9.1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cout << "Chislo:";
+	cin >> n;
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		cout << "a[" << i << "]= ";
+		cin >> arr[i];
+	}
+	cout << " a[ ";
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << " ]" << endl;
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum += arr[i] * arr[i];
+	}
+	cout << "sum= " << sum << endl;
+
+	delete[] arr;
+
+	return 0;
+}
