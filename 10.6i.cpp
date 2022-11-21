ca57f39ad207a15884165b36251ad6f25b35de8d
OlegChaichenko <113429945+OlegChaichenko@users.noreply.github.com>
2022-11-21 16:21:02 +0200

    Oleg Chaichenko,comp math 1,10.6i
    Oleg Chaichenko,comp math 1,10.6i

---
+++ 10.6i.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+#define N 5
+struct Vector {
+	double data[N];
+};
+Vector input_Vector() {
+	Vector example;
+	for (int i = 0; i < N; i++) {
+		cout << "a[" << i << "]" << endl;
+		cin >> example.data[i];
+	}
+	return example;
+}
+void show_Vector(Vector example){
+	for (int i = 0; i < N; i++) {
+		cout << "a[" <<i << "]=" << example.data[i] << endl;
+	}
+}
+
+int main(){
+	Vector ms;
+	ms = input_Vector();
+	show_Vector(ms);
+
+
+
+
+
+}
