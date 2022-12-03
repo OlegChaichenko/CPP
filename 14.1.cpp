d8ad15035dc71d0c15469894fbc4b9ae98dbf563
OlegChaichenko <113429945+OlegChaichenko@users.noreply.github.com>
2022-12-03 17:57:07 +0200

    Oleg Chaichenko,comp math 14.1-14.5,kr17
    Oleg Chaichenko,comp math 14.1-14.5,kr17

---
+++ 14.1.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void func_1(string name)
+{
+    fstream f;
+    f.open(name);
+    string a;
+    getline(f, a);
+    f.close();
+
+    int sum = 0;
+    int temp = 0;
+    int ttmp = 0;
+    string sub = "";
+    for (char b : a)
+    {
+        
+        if ((b != '+') && (b != '-') && (b != '='))
+        {
+            sub += b;
+        }
+        else
+        {
+            if (ttmp == 0)
+            {
+           
+                sum += stoi(sub);
+                ttmp = 1;
+                if (b == '-') { temp = -1; }
+                if (b == '+') { temp = 1; }
+                sub = "";
+            }
+            else
+            {
+                if (temp == -1)
+                {
+                    sum -= stoi(sub);
+                }
+                if (temp == 1)
+                {
+                    sum += stoi(sub);
+                }
+                sub = "";
+                if (b == '-') 
+                { temp = -1; }
+                if (b == '+') 
+                { temp = 1; }
+                if (b == '=') 
+                { temp = 0; break; }
+            }
+        }
+        if (b == '=')
+        {
+            break;
+        }
+    }
+
+    cout << sum << endl;
+
+}
+
+int main()
+{
+    func_1("test.txt");
+}
---
+++ 14.4.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+
+void func_1(string name)
+{
+    fstream f;
+    f.open(name, ios::in);
+
+    string temp;
+    string qwe = "<td>";
+    string qwe1 = "</td>";
+
+    int l = 0;
+    int k = 0;
+    while ((!f.eof()))
+    {
+        getline(f, temp);
+
+
+        if (temp.find(qwe) != -1) {
+            l += 1;
+            if (temp.find(qwe1) != -1) {
+                k += 1;
+
+
+            }
+        }
+        else if (temp.find(qwe1) != -1) {
+            k += 1;
+            if (temp.find(qwe) != -1) {
+                l += 1;
+            }
+
+        }
+        f.close();
+        if (l != k) {
+            cout << "TEGI NEPRAV" << endl;
+
+        }
+        else {
+
+            cout << "PRAVILNO" << endl;
+        }
+    }
+}
+
+
+
+int main()
+{
+    func_1("test.txt");
+}
