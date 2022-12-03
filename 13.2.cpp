caaa64da01f784c6d587fa062b8c10f680af8d19
OlegChaichenko <113429945+OlegChaichenko@users.noreply.github.com>
2022-12-03 17:55:08 +0200

    Oleg Chaichenko,comp math 13.1-13.6,kr16
    Oleg Chaichenko,comp math 13.1-13.6,kr16

---
+++ 13.2.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+//13_2
+
+using namespace std;
+
+string func_1(string a)
+{
+    int start = a.find('.');
+    int end = a.rfind('.');
+    //cout << start << " " << end << endl;
+    string b;
+    if ((start == end) && (end == -1))
+    {
+        b = a;
+        if (a[0] != ' ')
+        {
+            return b;
+        }
+        for (int i = 0; i < a.length(); i++)
+        {
+            if (a[i] != ' ')
+            {
+                b.erase(0, i - 1);
+            }
+            return b;
+        }
+    }
+    if ((start == end) && (end != -1))
+    {
+        b = a;
+        b.erase(0, end);
+        return b;
+    }
+    else
+    {
+        b = a;
+        b.erase(start + 1, a.length() - end);
+        return b;
+    }
+}
+
+int main()
+{
+    string a;
+    cout << "___.___.___:" << endl;
+    cin >> a;
+    string b = func_1(a);
+    cout << b << endl;
+}
